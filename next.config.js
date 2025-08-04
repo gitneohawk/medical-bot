75a876f590a3e168c07dd8be7bdaba6c5f8a6ccb
gitneohawk <neohawk@evoluzio.com>
2025-08-04 10:26:35 +0900

    yaml mod to avoid timeout

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-/** @type {import('next').NextConfig} */
+/ ** @type {import('next').NextConfig} * /
 const nextConfig = {
-  output: "export",
   reactStrictMode: true,
   // 必要に応じて他の設定を追加してください
 };
